4dc2a86fdf8c4e9b235ac69e65c023d4a761c579
Ritish jaiswal <ritishjaiswal20@gmail.com>
2022-01-11 16:37:50 +0530

    starting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { useEffect, useState } from 'react';
+import { doc, onSnapshot, getDocs,collection, query, where} from "firebase/firestore";
 import './App.css';
 import Post from './Post';
-import db from './firebase'
-import auth from './firebase'
-import storage from './firebase'
+import { getFirestore} from 'firebase/firestore';
+//  import {db}  from './firebase'
 import { Button, Input, makeStyles, Modal } from '@material-ui/core'
+import {onAuthStateChanged,signOut, getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword} from "firebase/auth";
+// import ImageUpload from './ImageUpload';
 
 function getModalStyle() {
   const top = 50;
   const left = 50;
 
   return {
-    top: `${top}%`,
+    top: `${top}%`,  
     left: `${left}%`,
     transform: `translate(-${top}%, -${left}%)`
   }
@@ -36,26 +38,128 @@ function App() {
   const [username,setUsername] = useState(false);
   const [password,setPassword] = useState(false);
   const [email,setEmail] = useState(false);
+  const [user,setUser]=useState(null);
+  const [openSignIn,setOpenSigIn]=useState(false);
+  const auth = getAuth();
+  const db=getFirestore();
+  // const db=getFirestore();
+  // const db=getFirestore();
   // useeffect runs a piece a code on specific condition 
+ 
+
   useEffect(()=>{
-      
-    db.collection('posts').onSnapshot(snapshot => {
-      // every single time change happens fire this line od code
-      setPosts(snapshot.docs.map(doc =>({
-        id: doc.id,
-        post: doc.data() 
-      })));
+    const auth = getAuth();
+    const unsubscribe=onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        console.log(user);
+        setUser(user);
+      if(user.displayName)
+      {
+        //dont update
+      }else{
+        return updateProfile(user, {
+          displayName:username,
+        });
+      }
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        setUser(null);
+      }
+    });
+
+    return ()=>{
+      unsubscribe();
+    }
+    },[user,username]);
+
+
+
+    useEffect(() => {
+      const q=query(collection(db,"posts"))
+       onSnapshot(q,(querySnapshot) => {
+        setPosts(querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          post: doc.data()
+        })))
+      })
+    }, [])
+
+    // useEffect(() => {
+    //    const q=query(collection(db,"posts"));
+    //    const querySnapshot= getDocs(q);
+    //     setPosts(querySnapshot.forEach(doc => ({
+    //       id: doc.id,
+    //       post: doc.data()
+    //     })))
+    //   },[])
+    
+    // useEffect(() => {
+    //   db.collection("posts").orderBy("timestamp", "desc").onSnapshot(snapshot => {
+    //     setPosts(snapshot.docs.map(doc => ({
+    //       id: doc.id,
+    //       post: doc.data()
+    //     })))
+    //   })
+    // }, [])
+  
+// useEffect(() => {
+
+//   const q = query(collection(db, "posts"));
+
+//   onSnapshot(q, (querySnapshot) => {
+//     setPosts(
+//       querySnapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() }))
+//     );
+//   });
+// });
+    
+ 
+  const signUp=(e) =>{
+    const auth = getAuth();
+    e.preventDefault();
+     createUserWithEmailAndPassword(auth,email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
     })
-  },[]);
-  const signUp=(event)=>{
-       event.preventDefault();
-       auth.createUserWithEmailAndPassword(email,password)
-       .catch((error)=> alert(error.message))
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(error.message);
+      // ..
+    });
+    setOpen(false);
   }
+
+const signIn=(e)=>{
+  const auth = getAuth();
+  e.preventDefault();
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(error.message);
+  });
+  setOpenSigIn(false);
+}
+
 
   return (
       
     <div className="app">
+      {/* <ImageUpload/> */}
       <Modal
       open={open}
       onClose={() =>setOpen(false)}
@@ -89,10 +193,56 @@ function App() {
      
      </div>
       </Modal>
+
+
+ 
+
+      <Modal
+      open={openSignIn}
+      onClose={() =>setOpenSigIn(false)}
+      >
+     <div style={modalStyle} className={classes.paper} >
+       <form className="app_signup">
+       <center>
+       <img className="app_headerImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png" width="200" height="100"></img>
+       </center>
+      
+        <Input
+         type="text"
+         placeholder="email"
+         value={email}
+         onChange={(e)=>setEmail(e.target.value)}
+       ></Input>   
+        <Input
+         type="password"
+         placeholder="password"
+         value={password}
+         onChange={(e)=>setPassword(e.target.value)}
+       ></Input>   
+     
+     <Button type="submit" onClick={signIn}>signIn</Button>
+     </form>
+     
+     </div>
+      </Modal>
+ 
+
+
+
+
        <div className="app_header">
         <img className="app_headerImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png" width="200" height="100"></img>    
-    </div>
-    <Button onClick={()=> setOpen(true)}>Sign out</Button>
+       </div>
+    {user ?(
+      <Button onClick={()=> signOut(auth)}>log out</Button>
+    ):(
+      <div className="app_loginConatiner">
+       <Button onClick={()=> setOpenSigIn(true)}>Sign In</Button> 
+      <Button onClick={()=> setOpen(true)}>Sign Up</Button>
+      </div>
+    )
+    }
+   
     {
       posts.map(({id,post})=> (
         <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
